998ff1e9bd96a8288e0e8e6844df2d572accdddb
Artur Grigorev <agrigorev@ethz.ch>
2025-02-28 17:23:15 +0100

    setup.sh updated: new comments; pytorch3d moved from requirements.txt

--- setup.sh
+++ setup.sh
@@ -12,6 +12,8 @@
 
 # install conda dependencies
 conda install pytorch==2.1.0 torchvision==0.16.0 torchaudio==2.1.0 pytorch-cuda=11.8 -c pytorch -c nvidia -y
+
+# install cuda and gcc inside the environment and set CUDA_HOME
 conda install cuda -c nvidia/label/cuda-11.8.0 -y
 conda install gcc=11.4.0 gxx=11.4.0 -y
 export CUDA_HOME=$CONDA_PREFIX
@@ -36,6 +38,9 @@ git clone https://github.com/graphdeco-inria/gaussian-splatting.git --recursive
 cd gaussian-splatting/submodules/simple-knn/
 python setup.py install
 cd ../../../..
+
+# install pytorch3d from source (may take a while)
+pip install git+https://github.com/facebookresearch/pytorch3d.git
 
 # install remaining requirements
 pip install -r requirements.txt
